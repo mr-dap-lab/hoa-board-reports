c4f957fc89400f599a2d4c83ca3b8de76c259278
Diego Fernando Avella Perez <d.avella@student.miuniversity.edu>
2025-08-24 23:59:03 -0400

    Add BoardReportsTemplate to App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,1 +1,9 @@
+import BoardReportsTemplate from "@/components/BoardReports";
 
+export default function App() {
+  return (
+    <main className="min-h-screen">
+      <BoardReportsTemplate branding={{ orgName: "HOA Space" }} />
+    </main>
+  );
+}
